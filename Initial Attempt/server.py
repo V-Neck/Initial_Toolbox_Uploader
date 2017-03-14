3c75ae4e28e5b6fc5cb4c604320e0b29fc30a983
Vaughn Johnson <evilepicproductions@gmail.com>
2017-03-14 15:12:47 -0700

    Miscilaneous things

--- Initial Attempt/server.py
+++ Initial Attempt/server.py
@@ -5,12 +5,9 @@ UPLOAD_FOLDER = "temp"
 tempfile.tempdir = UPLOAD_FOLDER
 CHARS = "abcdefghijklmnopqrstuvwxyz0123456789"
 TOOLBOX_LINE_PATTERN = re.compile(r"^\\[a-z]+")
-TOOLBOX_STD_MKR = {'\\t': 'text',
-    '\\ref': 'reference',
-    '\\f': 'free translation',
-    '\\g': 'gloss',
-    '\\l': 'literal translation',
-    '\\m': 'morphemes'}
+TB_STD_MKRS = ['\\ref', '\\t', '\\m','\\g', '\\f', '\\l']
+TB_STD_MKR_INTERP = ['reference', 'text', 'morphemes', 'gloss','free translation', 'literal translation']
+TOOLBOX_STD_MKR = dict(zip(TB_STD_MKRS, TB_STD_MKR_INTERP))
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -28,14 +25,14 @@ def upload(name=None):
         os.remove(path)
         records = toolbox.records(lines, ['\\id', '\\ref'])
         rec1 = next(records)
+        #How get ref into this?
         pairs = [_ for _ in toolbox.normalize_record(rec1[1], ['\\t', '\\g', '\\m'])]
 
         if uses_std_mkrs([x[0] for x in pairs]):
             return render_template("success.html", pairs=pairs[:len(TOOLBOX_STD_MKR)], mkr_format=TOOLBOX_STD_MKR)
         else:
-            non_std_mkrs = get_odd_mkrs([x[0] for x in pairs])
-            os.remove(path)
-            return render_template("non_std_fail.html", mkr_format=TOOLBOX_STD_MKR, non_std_mkrs=non_std_mkrs)
+            non_std_mkrs = get_non_std_mkrs([x[0] for x in pairs])
+            return render_template("non_std_fail.html", mkr_format=TB_STD_MKRS, non_std_mkrs=non_std_mkrs)
 
     return render_template("upload.html", name=name)
 
@@ -50,22 +47,48 @@ def rand_string(path, N):
     return file_name
 
 #determines whether the user used standard keys
-def uses_std_mkrs(mkrs):
-    for mkr in mkrs:
-        if mkr not in TOOLBOX_STD_MKR.keys():
+#Quick and dirty method just checks if they ever used a non standard marker
+#fuller method also checks for order
+def uses_std_mkrs(mkrs, quick_and_dirty=False):
+    if quick_and_dirty:
+        for mkr in mkrs:
+            if mkr not in TB_STD_MKRS():
+                return False
+        return True
+    else:
+        #hacky temporary fix to the problem of pairs not including "ref"
+        if len(mkrs) != (len(TB_STD_MKRS) -1):
             return False
-    return True
+        i = 0
+        while i < len(mkrs) and i < len(TB_STD_MKRS):
+            if not mkrs[i] == TB_STD_MKRS[i]:
+                return False
+            i += 1
+        print mkrs, TB_STD_MKRS
+        print i
+        return True
 
-def get_odd_mkrs(mkrs):
+#determines whether upload used standard keys in standard order
+def get_non_std_mkrs(mkrs):
+    print mkrs
+    print  TB_STD_MKRS
     non_std_mkrs = []
-    for mkr in mkrs:
-        if mkr not in TOOLBOX_STD_MKR:
-            print mkr
-            non_std_mkrs.append(mkr)
+    i = 0
+    #hacky temporary fix to the problem of pairs not including "ref"
+    while i < len(mkrs) and i < (len(TB_STD_MKRS)-1):
+        #hacky temporary fix to the problem of pairs not including "ref"
+        if not mkrs[i] == TB_STD_MKRS[i+1]:
+            non_std_mkrs.append(mkrs[i])
+        i += 1
     return non_std_mkrs
 
 def find_delimiter(lines):
-    pass
+    block_initial_marker = lines[0].split(" ")[0]
+    for i in range(1, len(lines)):
+        if lines[i] == block_initial_marker:
+            print lines[i]
+            return lines[i-1]
+    return "\n\n"
 
 if __name__ == "__main__":
     app.run(debug=True)
